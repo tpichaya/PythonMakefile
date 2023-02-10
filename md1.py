7701bfc291c71b33d8020b75ffd29a601f3084d0
Pichaya Tandayya <43403592+tpichaya@users.noreply.github.com>
2023-02-10 13:18:18 +0700

    Create md1.py

---
+++ md1.py
@@ -0,0 +1,14 @@
+def f1(n) :
+    x = n+100
+    return x
+def f2(x,y) :
+    z = x+2*y
+    return z
+def main() :
+    print('md1')
+    x=f1(2); y=f2(x,3)
+    print(y,'\nEnd of md1’)
+
+main()    
+#if __name__ == "__main__" :
+#    main()
